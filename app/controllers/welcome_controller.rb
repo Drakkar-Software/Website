bc55847f046d85ee49e32de61cb15f8da7d1643c
Herklos <herklos.dev@protonmail.com>
2019-05-05 16:40:13 +0200

    Implement counters

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
+  include WelcomeHelper
+
   def index
     @articles = Article.all.order(published_at: :desc)
+
+    @total_active_bots = metrics
+    puts @total_active_bots
+    @month_active_bots = metrics(month = -1)
+    puts @month_active_bots
   end
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,11 @@
+require 'net/http'
+
 module WelcomeHelper
+  def metrics(year = 0, month = 0, day = 0)
+    uri = URI.parse("https://octobotmetrics.herokuapp.com/metrics/community/count/#{year}/#{month}/#{day}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    @data = http.get(uri.request_uri)
+    JSON.parse(@data.body)["total"]
+  end
 end
