99fa4f2f499015e441097ee2828b93f123120f73
herklos <herklos.dev@protonmail.com>
2019-11-20 23:14:15 +0100

    Added config host

--- docker/wait-for-services.sh
+++ docker/wait-for-services.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
 # Wait for PostgreSQL
-#until nc -z -v -w30 $DATABASE_HOST $DATABASE_PORT
-#do
-#  echo 'Waiting for PostgreSQL...'
-#  sleep 1
-#done
+until nc -z -v -w30 $DATABASE_HOST $DATABASE_PORT
+do
+  echo 'Waiting for PostgreSQL...'
+  sleep 1
+done
 echo "PostgreSQL is up and running"
